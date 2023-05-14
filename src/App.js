64a4a5e26c6b32c7a20c17f2cd0276597780e1fc
Jitender <jitendersharmateharki@gmail.com>
2023-05-14 18:16:35 +0530

    Restaurant app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@
 import React, {useState} from 'react'
 
 import Resturant from './component/Basics/Resturant'
-import UseState from './component/Hooks/useState'
-import UseEffect from './component/Hooks/useEffect'
+// import UseState from './component/Hooks/useState'
+// import UseEffect from './component/Hooks/useEffect'
 // import UseState from './component/Hooks/Usestate' // wrong method
-import UseReducer from './component/Hooks/useReducer'
+// import UseReducer from './component/Hooks/useReducer'
 
 
 
 const App = () => {
   return (
     <>
-      {/* <Resturant/> */}
+      <Resturant/>
       {/* <UseState/> */}
       {/* <UseEffect/> */}
-      <UseReducer/>
+      {/* <UseReducer/> */}
     </>
   )
 }
