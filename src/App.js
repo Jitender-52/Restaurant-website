d5d8329ec2b62771ddb74864b0861e9956f7f772
Jitender <jitendersharmateharki@gmail.com>
2023-07-04 10:23:57 +0530

    Favicon updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 // rafce
 import React, {useState} from 'react'
 
-import Resturant from './component/Basics/Resturant'
+// import Resturant from './component/Basics/Resturant'
 // import UseState from './component/Hooks/useState'
 // import UseEffect from './component/Hooks/useEffect'
 // import UseState from './component/Hooks/Usestate' // wrong method
 // import UseReducer from './component/Hooks/useReducer'
+import Todo from './component/todoreact/todo'
 
 
 
 const App = () => {
   return (
     <>
-      <Resturant/>
+      {/* <Resturant/> */}
       {/* <UseState/> */}
       {/* <UseEffect/> */}
       {/* <UseReducer/> */}
+      <Todo/>
     </>
   )
 }
---
+++ src/component/todoreact/todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./style.css";
+
+const Todo = () => {
+  return (
+    <>
+        <div className="main-div">
+            <div className="child-div">
+                <figure>
+                    <img src="./images/todo.svg" alt="todologo" />
+                    <figcaption>Add your list here ✌️</figcaption>
+                </figure>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Todoz
